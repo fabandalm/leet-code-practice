0370f7fd6c62d70c52591aef9de48fbfdf37711c
Fabio Almeida <falmeida.tech@gmail.com>
2020-12-31 15:42:04 -0500

    Added a new stack sample

--- src/main/java/com/falmeida/tech/RegexSample.java
+++ src/main/java/com/falmeida/tech/RegexSample.java
@@ -7,13 +7,14 @@ public class RegexSample {
     public static void main(String[] args) {
         String text    =
                 "This is the text to be searched " +
-                        "for occurrences of the http:// pattern.";
+                        "for occurrences of the http:// pattern. http://lll";
 
         String regex = ".*http://.*";
 
         boolean matches = Pattern.matches(regex, text);
 
         System.out.println("matches = " + matches);
+
     }
 
 }
---
+++ src/main/java/com/falmeida/tech/collections/StackSample.java
@@ -0,0 +1,12 @@
+package com.falmeida.tech.collections;
+
+import java.util.Stack;
+
+public class StackSample {
+
+    public static void main(String[] args) {
+        Stack<String> stack = new Stack();
+
+    }
+
+}
