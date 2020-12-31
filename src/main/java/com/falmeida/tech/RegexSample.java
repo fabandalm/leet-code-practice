48c780a6ba1e46427e7a9bdcfbc6f01d50afc7fa
Fabio Almeida <falmeida.tech@gmail.com>
2020-12-31 14:00:08 -0500

    Added simple example of regular expressions

---
+++ src/main/java/com/falmeida/tech/RegexSample.java
@@ -0,0 +1,19 @@
+package com.falmeida.tech;
+
+import java.util.regex.Pattern;
+
+public class RegexSample {
+
+    public static void main(String[] args) {
+        String text    =
+                "This is the text to be searched " +
+                        "for occurrences of the http:// pattern.";
+
+        String regex = ".*http://.*";
+
+        boolean matches = Pattern.matches(regex, text);
+
+        System.out.println("matches = " + matches);
+    }
+
+}
