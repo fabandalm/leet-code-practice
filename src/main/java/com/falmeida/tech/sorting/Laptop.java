d8d16fdd312aba8299649281a5bb566dc3046ee6
Fabio Almeida <falmeida.tech@gmail.com>
2021-02-04 23:46:28 -0500

    Added a pojo class

---
+++ src/main/java/com/falmeida/tech/sorting/Laptop.java
@@ -0,0 +1,41 @@
+package com.falmeida.tech.sorting;
+
+public class Laptop {
+
+    private String brand;
+    private int ram;
+    private int price;
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public int getRam() {
+        return ram;
+    }
+
+    public void setRam(int ram) {
+        this.ram = ram;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Laptop{" +
+                "brand='" + brand + '\'' +
+                ", ram=" + ram +
+                ", price=" + price +
+                '}';
+    }
+}
